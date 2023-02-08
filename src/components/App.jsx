5689706af68f5e0662290d84d4a06d6bd8cde708
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-02-08 14:36:36 +0200

    Added defineDiceNumbers function that returns an array of random numbers and also created allDice variable that consists of all Dice elements. Had rendered all dice elements.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,25 @@ import "../styles/App.css";
 import { Dice } from "./Dice.jsx";
 
 export default function App() {
+  const [diceNumbers, setDiceNumbers] = React.useState(defineDiceNumbers());
+
+  function defineDiceNumbers() {
+    const diceNumbers = [];
+
+    for (let i = 0; i < 10; i++) {
+      diceNumbers.push(Math.floor(Math.random() * 6) + 1);
+    }
+
+    return diceNumbers;
+  }
+
+  const allDice = diceNumbers.map(diceNumber => <Dice value={diceNumber} />);
+
   return (
     <div className="container">
       <main className="main__content">
         <section className="dice__container">
-          <Dice value={1} />
+          {allDice}
         </section>
       </main>
     </div>
